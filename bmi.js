638dd5b0e843ff72dbca1a755ea0970430c5c433
yutai934357 <luffy2015813@gmail.com>
2025-09-23 15:30:06 +0800

    111

--- bmi.js
+++ bmi.js
@@ -1,4 +1,5 @@
 const readline=requier('readline-sync');
+
 var weight=readline.question("Enter your weight in kg:");
 var height=readline.uestion("Enter your height in cm:");
 var bmi=weight/((height/100)**2)
