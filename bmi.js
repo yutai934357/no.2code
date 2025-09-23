8bcb26a84ca3653ac7286bba1e86e9b367964fff
yutai934357 <luffy2015813@gmail.com>
2025-09-23 16:03:03 +0800

    最後儲存01

--- bmi.js
+++ bmi.js
@@ -1,7 +1,6 @@
 const readline = require('readline-sync');
-
-var weight = readline.question("Enter your weight in kg:");
+var weight=readline.question("Enter your weight in kg:");
 var height = readline.question("Enter your height in cm:");
-var bmi = weight / ((height / 100) ** 2)
+var bmi=weight/((height/100)**2)
 
 console.log("Hello! Your BMI is:" + bmi)
